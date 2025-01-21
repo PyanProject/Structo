e5bf7420ab5ec6dd6a39bb31f99a49054a7c2c1d
alttt. <super-alt.net@yandex.ru>
2025-01-22 02:09:45 +0300

    fix footer on info page

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main():
     # Обучение
     print("[MAIN] Запуск обучения...")
     try:
-        train_gan(generator, discriminator, dataloader, embedding_generator, epochs=10, lr=0.0001, device=device)
+        train_gan(generator, discriminator, dataloader, embedding_generator, epochs=1, lr=0.0002, device=device)
         print("[MAIN] Обучение завершено.")
     except Exception as e:
         print(f"[MAIN] Ошибка обучения: {e}")
@@ -122,7 +122,8 @@ def main():
             noise = torch.randn(1, generator.noise_dim).to(device)
             generated_data = generator(noise, embedding).cpu().detach().numpy().squeeze()
 
-        scene_filename = generate_3d_scene_from_embedding(generated_data, text)
+        vertices, faces = dataset_generator[0]
+        scene_filename = generate_3d_scene_from_embedding(generated_data, text, faces.numpy())
         print(f"[MAIN] Модель сохранена: {scene_filename}")
     except Exception as e:
         print(f"[MAIN] Ошибка генерации: {e}")
